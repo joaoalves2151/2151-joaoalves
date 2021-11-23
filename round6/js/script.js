775676e088a1e08f10f7391389761401e8962baf
Mateus <joao.alves.estudante@eteot.rio.br>
2021-11-23 17:42:51 -0300

    att 8.0

--- round6/js/script.js
+++ round6/js/script.js
@@ -2,3 +2,6 @@ function fechar(){
     document.getElementById('popup').style.display = "none";
 }
 
+function aparecer(){
+                    document.getElementById('fechar').style.opacity="1"
+}
