bf5273ceaae28f97260dfd7913de29dd46720daf
joaoalves2151 <joao.alves.estudante@eteot.rio.br>
2022-02-09 22:41:38 -0300

    mod1.0

--- round6/js/script.js
+++ round6/js/script.js
@@ -1,17 +1,47 @@
-function fechar(){
-    document.getElementById('popup').style.display = "none";
-}
+// Botão fechar
+$(document).ready(function(){
+            $('#btf').click(function(fechar){
+                $('#popup').css({'display' : 'none'}); 
+            });
 
-function aparecer(){
-                    document.getElementById('fechar').style.opacity="1"
-}
 
-function verificar(){
-                     aceitar = document.querySelector('input:checked')
-                     
-                     if(aceitar != null){
-                        window.location.href='loc.html'
-                     }else{
-                         alert('Aceite os termos para prosseguir!')
-                     }
-}
+    //aparecer o botão de fechar '#btf'
+    $('#dap').on(function(aparecer){
+            
+            apr = $(this).mousemove()
+
+                    if ( apr == false  ) {
+                        $('#btf').hide()
+                    }
+                    
+                        else{
+                            $('#btf').show()
+                            }
+    });
+
+
+    //checar
+    $('#cx').click(function(checar){
+            
+                aceitar = (this).querySelector('input:checked')
+
+                    if (aceitar != null ){  
+                    $(location).href('loc.html')
+                    //ou window.location.href = 'loc.html'
+                    }
+
+                        else {
+                            alert("Você precisa aceitar os termos do contrato para prosseguir!")
+                        }
+    });
+
+                //$('fechar'). mousemove(function(aparecer){
+                // $(this).css({'opacity' : '1'});
+                //})
+
+});
+
+
+
+
+
